0f63dc2f92f8f3245df19296024db17af6b40237
Carolina Limeres <carolinalimeres@outlook.com>
2024-12-02 16:49:56 +0000

    update portfolio

--- components/header.js
+++ components/header.js
@@ -8,17 +8,34 @@ class Header extends HTMLElement {
         <style>
         .header {        
           nav {
-            height: 40px;
+            height: 60px;
             width: 100%;
             display: flex;
             align-items: center;
             justify-content: center;
-            background-color:  #0a0a23;
+             background-color: rgba(234, 234, 234, 0.7);
+             backdrop-filter: blur(10px);
           }
   
           ul {
             padding: 0;
             display: flex;
+            align-items: center;
+            justify-content: space-around;
+          }
+
+          .nav-header-container {
+          display: flex;
+           align-items: center;
+           justify-content: space-between;
+          }
+
+          .logo-header {
+          width: 100px
+          }
+
+          .items-header-container {
+          display: flex;
           }
   
           a {
@@ -37,11 +54,18 @@ class Header extends HTMLElement {
         <header class="header">
           <nav>
             <ul>
+            <div class="nav-header-container">
+            <div class="logo-header-container">
+            <li><a href="/index.html"><img src="/components/logo.png" class="logo-header" alt="" /></a></li>
+            </div>
+            <div class="items-header-container">
               <li><a href="/index.html">About</a></li>
               <li><a href="/pages/blog/blog.html">Blog</a></li>
               <li><a href="/pages/contact/contact.html">Contact</a></li>
               <li><a href="/pages/services/services.html">Services</a></li>
-              <li><a href="/pages/portfolio/portfolio.html">Portfolio</a></li>              
+              <li><a href="/pages/portfolio/portfolio.html">Portfolio</a></li> 
+              </div>
+              </div>
             </ul>
           </nav>
         </header>
